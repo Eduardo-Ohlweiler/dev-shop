8d1e94135a81862e242c52b6e56bf865cbc0ed8e
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2025-04-11 21:57:00 -0300

    feat:adiciona estrutura inicial da home

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,31 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Group, Heading, Input, Text } from "@chakra-ui/react";
+import { IoIosSearch } from "react-icons/io";
 
 export default function Home() {
   return (
-    <Center>
-      <Heading >Teste</Heading>
+    <Center flexDir="column">
+      <Heading pt="2rem" >Bem vindo ao DevShop</Heading>
+      <Group attached w="4xl" mt="2rem" >
+        <Input placeholder="Buscar..." />
+        <Button bg="bg.subtle" variant="outline" >
+          <IoIosSearch />
+        </Button>
+      </Group>
+      <Box w="60%" h="500px" bg="gray.600" mt="2rem" ></Box>
+      
+      <Box mt="3rem" >
+        <Heading textAlign="center" mb="1rem" >Categorias</Heading>
+        <Flex gap="2rem" >
+          {
+            ["Roupas", "Perifericos", "Utencilios"].map((item, index) => (
+                <Box key={index} textAlign="center" >
+                    <Box w="70px" h="70px" bg="gray.300" borderRadius="full" ></Box>
+                    <Text>{item}</Text>
+                </Box>
+            ))
+          }
+        </Flex>
+      </Box>
     </Center>
   );
 }
