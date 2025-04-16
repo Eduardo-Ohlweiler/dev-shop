600296cb11a229c0dade5c6dde077b589b2aad68
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2025-04-15 21:33:20 -0300

    feat:conclui formularios de login e cadastros

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,16 +1,51 @@
 "use client";
 
-import { AuthContextInterface, RegisterUserData } from "@/types/auth";
+import { loginUserService, registerUserService } from "@/services/auth.service";
+import { AuthContextInterface, LoginUserData, RegisterUserData } from "@/types/auth";
+import { useRouter } from "next/navigation";
 import React,{ createContext, useContext } from "react";
+import { toast } from "react-toastify";
 
 const AuthContext = createContext<AuthContextInterface>({} as AuthContextInterface);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    
+    const router = useRouter();
 
-    const registerUser = (data: RegisterUserData) => {};
+    const registerUser = async (data: RegisterUserData) => 
+    {
+        try
+        {
+            const response = await registerUserService(data);
+            localStorage.setItem("@token", JSON.stringify(response.accessToken));
+            toast.success("Cadastro realizado com sucesso!");
+            router.push("/login");
+        }  
+        catch (error: any) 
+        {
+            toast.error(error.response.data);
+        }  
+        
+    };
+
+    const loginUser = async (data: LoginUserData) => 
+    {
+        try
+        {
+            const response = await loginUserService(data);
+            console.log(response);
+            localStorage.setItem("@token", JSON.stringify(response.accessToken));
+            router.push("/");
+            toast.success("Login realizado com sucesso!");
+        }
+        catch (error: any) 
+        {
+            toast.error(error.response.data);
+        }
+    };
     
     return (
-        <AuthContext.Provider value={{registerUser}}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={{registerUser, loginUser}}>{children}</AuthContext.Provider>
     );
 };
 
