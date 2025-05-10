8e4b6703cc66c7d63b4fc2ce6690c21060a7fa42
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2025-05-10 15:33:17 -0300

    feat: adiciona contexto do carrinho

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,8 +1,12 @@
-
+"use client"
+import { useCart } from "@/contexts/CartContext";
 import { Product } from "@/types/products";
 import { Box, Button, Card, Image, Text } from "@chakra-ui/react"
 
 const ProductCard: React.FC<Product> = ({ id, name, value, category_id, img_src}) => {
+
+  const {addToCart} = useCart();
+
   return (
     <Card.Root  overflow="hidden">
         <Box h="350px" bg="white">
@@ -20,7 +24,11 @@ const ProductCard: React.FC<Product> = ({ id, name, value, category_id, img_src}
         <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
           {value.toLocaleString("pt-BR", {currency: "BRL", style: "currency"})}
         </Text>
-        <Button variant="ghost" w="100%" bg={"white"} color={"black"}>Adicionar</Button>
+        <Button variant="ghost" w="100%" bg={"white"} color={"black"}
+          onClick={() => addToCart({id, name, value, category_id, img_src})}
+        >
+          Adicionar
+        </Button>
       </Card.Footer>
     </Card.Root>
   )
--- src/types/products.d.ts
+++ src/types/products.d.ts
@@ -1,3 +1,4 @@
+import { CartCardProps } from "@/components/CartCard";
 
 export interface Product {
     id: number;
@@ -5,4 +6,12 @@ export interface Product {
     value: number;
     category_id: number;
     img_src: string;
+}
+
+export interface CartContextInterface {
+    cartList: CartCardProps[];
+    addToCart: (Product) => void;
+    removeToCart: (id) => void;
+    clearCart: () => void;
+    total: number;
 }
